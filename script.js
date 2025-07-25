bad16efb38989f009d76a4eed523a6cf8930c35c
Yuurij-dev <yurijesus.dev@gmail.com>
2025-07-25 02:49:19 -0300

    Feat Mobile version Finished

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const swiper = new Swiper('.swiper', {
-      loop: true,
-      autoplay: {
-        delay: 3000,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function toggleAnswer(id) {
+  const answer = document.getElementById(id);
+  answer.classList.toggle('hidden');
+}
